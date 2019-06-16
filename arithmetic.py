34cf5939193c05e751eca414f2904d56d0a37860
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-16 11:31:43 -0400

    Image arithmetic.

--- arithmetic.py
+++ arithmetic.py
@@ -15,3 +15,15 @@ print(f'Min of 0: {cv2.subtract(np.uint8([50]), np.uint8([100]))}.')
 
 print(f'Wrap around: {np.uint8([200]) + np.uint8([100])}.')
 print(f'Wrap around: {np.uint8([50]) - np.uint8([100])}')
+
+# Image becomes brighter because we are increasing pixel values.
+M = np.ones(image.shape, dtype='uint8') * 100
+added = cv2.add(image, M)
+cv2.imshow('Added via OpenCV', added)
+cv2.waitKey(0)
+
+# Image becomes darker because we are decreasing pixel values.
+M = np.ones(image.shape, dtype='uint8') * 50
+subtracted = cv2.subtract(image, M)
+cv2.imshow('Subtracted via OpenCV', subtracted)
+cv2.waitKey(0)
