0d7cab31bf2e040af8507166c4315d2d3738d710
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-15 23:33:00 -0400

    Arithmetic with OpenCV operations.

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='Path to the image.')
+args = vars(ap.parse_args())
+
+image = cv2.imread(args['image'])
+cv2.imshow('Original', image)
+cv2.waitKey(0)
+
+print(f'Max of 255: {cv2.add(np.uint8([200]), np.uint8([100]))}.')
+print(f'Min of 0: {cv2.subtract(np.uint8([50]), np.uint8([100]))}.')
