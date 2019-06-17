7abbad670fd7a7fb2faf661a41119b1307df1cbd
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-17 08:37:39 -0400

    Alternative visualization of channels with color.

--- splitting_and_merging.py
+++ splitting_and_merging.py
@@ -9,8 +9,8 @@ args = vars(ap.parse_args())
 image = cv2.imread(args['image'])
 (B, G, R) = cv2.split(image)
 
+# Darker areas in a channel means there is not much of that color in that respective channel.
 cv2.imshow('RED', R)
-cv2.moveWindow('RED', 20, 20)
 cv2.imshow('GREEN', G)
 cv2.imshow('BLUE', B)
 cv2.waitKey(0)
@@ -19,3 +19,12 @@ merged = cv2.merge([B, G, R])
 cv2.imshow('Merged Image', merged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# An alternative way to visualize the channels is by setting the intensity of the channel pixels into its respective channel while setting the other channels to zero.
+zeros = np.zeros(image.shape[:2], dtype='uint8')
+cv2.imshow('RED', cv2.merge([zeros, zeros, R]))
+cv2.imshow('GREEN', cv2.merge([zeros, G, zeros]))
+cv2.imshow('BLUE', cv2.merge([B, zeros, zeros]))
+cv2.waitKey(0)
+
+# Converting color spaces.
