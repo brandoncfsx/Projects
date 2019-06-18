328e7fce76c8f423a38e98752cb3311176b046c2
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-18 19:46:04 -0400

    Created histograms for each channel on one figure.

---
+++ color_histograms.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='Path to the image.')
+args = vars(ap.parse_args())
+
+image = cv2.imread(args['image'])
+cv2.imshow('Original', image)
+
+chans = cv2.split(image)
+colors = ('b', 'g', 'r')
+plt.figure()
+plt.title('Flattened Color Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of Pixels')
+
+# For each channel we compute a histogram and setting the color attribute of the plot to the respective channel.
+for (chan, color) in zip(chans, colors):
+    hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+    plt.plot(hist, color=color)
+    plt.xlim([0, 256])
+    
+plt.show()
+cv2.waitKey(0)
