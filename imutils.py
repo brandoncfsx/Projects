873f1ab956a6e3ba5e35a0698ec554d05b94989f
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-18 21:48:03 -0400

    Added docstrings.

--- imutils.py
+++ imutils.py
@@ -2,11 +2,20 @@ import numpy as np
 import cv2
 
 def translate(image, x, y):
+    """
+    Translate image horizontally and/or vertically.
+    """
     M = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
     return shifted
 
 def rotate(image, degrees, scale=1.0, center=None):
+    """
+    Rotate and scale an image.
+
+    Keyword arguments:
+    center -- location from which we will rotate the image.
+    """
     (h, w) = image.shape[:2]
 
     if center is None:
@@ -17,6 +26,12 @@ def rotate(image, degrees, scale=1.0, center=None):
     return rotated
 
 def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    """
+    Resize the image while maintaining aspect ratio.
+
+    Keyword arguments:
+    inter -- method of interpolation.
+    """
     if height == None:
         r = width / image.shape[1]
         dim = (width, int(image.shape[0] * r))
