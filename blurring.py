74f5d7cbc55b9a08b4fb730066da0006e89053ac
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-20 08:51:39 -0400

    Added Gaussian blurring.

--- blurring.py
+++ blurring.py
@@ -9,11 +9,23 @@ args = vars(ap.parse_args())
 image = cv2.imread(args['image'])
 cv2.imshow('Original', image)
 
-# Averaging Method.
+# Averaging Blurring.
+# hstack: stacks images together horizontally. Allows us to use one window to display all three images.
+# Blur args: image and size of kernel.
 blurred = np.hstack([
     cv2.blur(image, (3, 3)),
     cv2.blur(image, (5, 5)),
     cv2.blur(image, (7, 7))])
 
 cv2.imshow('Averaged', blurred)
+cv2.waitKey(0)
+
+# Gaussian Blurring.
+# Args for GaussianBlur is the same for the averaging blurring except the third argument is our standard deviation in the x-axis direction. Setting this value to zero lets OpenCV automatically compute it based on our kernel size.
+blurred = np.hstack([
+    cv2.GaussianBlur(image, (3, 3), 0),
+    cv2.GaussianBlur(image, (5, 5), 0),
+    cv2.GaussianBlur(image, (7, 7), 0)])
+
+cv2.imshow('Gaussian', blurred)
 cv2.waitKey(0)
