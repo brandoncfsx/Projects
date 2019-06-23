9bc787fd6f59a664243e339acf088df10aedb9a0
Brandon Chung <brandoncfsx@yahoo.com>
2019-06-23 00:45:30 -0400

    Implemented Canny Edge Detector.

---
+++ canny.py
@@ -0,0 +1,17 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='Path to the image.')
+args = vars(ap.parse_args())
+
+image = cv2.imread(args['image'])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow('Blurred', image)
+
+# Args: blurred, grayscale image; then two values - threshold1 and threshold2. Any gradient value larger than threshold2 is considered to be an edge. Any value lower than threshold1 is considered not to be an edge. Values in between thresholds are either classified as edges or non-edges based on how their intensities are connected.
+canny = cv2.Canny(image, 30, 150)
+cv2.imshow('Canny', canny)
+cv2.waitKey(0)
