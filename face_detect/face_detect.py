715984523fbba9dfc21c7be9b24375444bab6a98
Brandon Chung <brandoncfsx@yahoo.com>
2019-08-09 16:56:28 -0400

    Added pre-trained DNN and its weights.

--- face_detect/face_detect.py
+++ face_detect/face_detect.py
@@ -19,3 +19,29 @@ image = cv2.imread(args['image'])
 (h, w) = image.shape[:2]
 # Preprocess image - sets blob dimensions and normalization.
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+# Pass the blob through the DNN and grab the detections.
+print('COMPUTING OBJECT DETECTIONS ...')
+net.setInput(blob)
+detections = net.forward()
+
+# Loop over the detections.
+for i in range(0, detections.shape[2]):
+    # Extract confidence associated with the detection.
+    confidence = detections[0, 0, i, 2]
+
+    # Filter out weak detections.
+    if confidence > args['confidence']:
+        # Compute (x, y) coordinates of the bounding box.
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype('int')
+
+        # Draw bounding box of the face along with its confidence.
+        text = f'{(confidence * 100)}.2f%'
+        # Conditional to ensure text will not appear above the image.
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+cv2.imshow('Output', image)
+cv2.waitkey(0)
